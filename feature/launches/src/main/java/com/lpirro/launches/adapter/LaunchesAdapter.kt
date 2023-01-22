7d2bd039507a744c70dd472e5024ed3d22f5bc93
lpirro <leonardo.pirro@wombatinvest.com>
2023-01-22 20:24:39 +0100

    Added update Icon

--- feature/launches/src/main/java/com/lpirro/launches/adapter/LaunchesAdapter.kt
+++ feature/launches/src/main/java/com/lpirro/launches/adapter/LaunchesAdapter.kt
@@ -26,9 +26,7 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import com.bumptech.glide.request.RequestOptions
 import com.lpirro.domain.models.Launch
 import com.lpirro.domain.models.Status
 import com.lpirro.launches.CountdownTimerTextView
